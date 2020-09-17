52a1d9922c547663751adfbd7d527f30fbda8f88
jossrc <kuro.rc10@gmail.com>
2020-09-17 11:58:53 -0500

    Permitiendo insertar los valores de los txt a la tabla

--- CasoEstudio_01/Form1.cs
+++ CasoEstudio_01/Form1.cs
@@ -12,6 +12,8 @@ namespace CasoEstudio_01
 {
     public partial class Form1 : Form
     {
+        List<Libro> libros = new List<Libro>();
+
         public Form1()
         {
             InitializeComponent();
@@ -26,6 +28,15 @@ namespace CasoEstudio_01
             book.AñoEdicion = Int32.Parse(txtAñoEdicion.Text);
             book.Estado = txtEstado.Text;
             book.Sumilla = txtSumilla.Text;
+
+            libros.Add(book);
+
+            int n = dgLibro.Rows.Add();
+
+            dgLibro.Rows[n].Cells[0].Value = txtTitulo.Text;
+            dgLibro.Rows[n].Cells[1].Value = txtAutor.Text;
+            dgLibro.Rows[n].Cells[2].Value = txtAñoEdicion.Text;
+            dgLibro.Rows[n].Cells[3].Value = txtEstado.Text;
         }
     }
 }
