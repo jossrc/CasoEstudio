b0c029e7814cabde8077344b4f4c3439b2ddf2d0
jossrc <kuro.rc10@gmail.com>
2020-09-21 13:52:43 -0500

    Creando la clase BestSeller

---
+++ CasoEstudio_01/Clases/BestSeller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasoEstudio_01.Clases
+{
+    public class BestSeller: Publicacion
+    {
+        private string sumilla;
+
+        public BestSeller(string titulo, string autor, int añoEdicion, string estado, string sumilla)
+            :base(titulo, autor, añoEdicion, estado)
+        {
+            this.Sumilla = sumilla;
+        }
+
+        public string Sumilla { get => sumilla; set => sumilla = value; }
+    }
+}
