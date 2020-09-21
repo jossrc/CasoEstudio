28b53712c87ac4d55d80f149291af50ff3c491cc
jossrc <kuro.rc10@gmail.com>
2020-09-21 13:56:45 -0500

    Creando la clase hija Libro

---
+++ CasoEstudio_01/Clases/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasoEstudio_01.Clases
+{
+    public class Libro:Publicacion
+    {
+        private string sumilla;
+
+        public Libro(string titulo, string autor, int añoEdicion, string estado, string sumilla)
+            :base(titulo, autor, añoEdicion, estado)
+        {
+            this.Sumilla = sumilla;
+        }
+
+        public string Sumilla { get => sumilla; set => sumilla = value; }
+    }
+}
