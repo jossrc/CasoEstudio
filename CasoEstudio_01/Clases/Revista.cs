496dd1818ec894bf4084857feaf1e060f4fb6e1e
jossrc <kuro.rc10@gmail.com>
2020-09-21 13:50:16 -0500

    Creando la clase Revista

---
+++ CasoEstudio_01/Clases/Revista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasoEstudio_01.Clases
+{
+    public class Revista: Publicacion
+    {
+        public Revista(string titulo, string autor, int añoEdicion, string estado)
+            :base(titulo, autor, añoEdicion, estado)
+        {
+
+        }
+    }
+}
