b3010c672b3b0ca5ab753f96e60d864600f1ff19
jossrc <kuro.rc10@gmail.com>
2020-09-21 14:29:33 -0500

    Creando un combobox para el tipo y reacomodando el diseño del Form

--- CasoEstudio_01/Form1.Designer.cs
+++ CasoEstudio_01/Form1.Designer.cs
@@ -45,6 +45,8 @@
             this.colAño = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colEstado = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnEliminar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgLibro)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +54,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Location = new System.Drawing.Point(28, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 25);
             this.label1.TabIndex = 0;
@@ -62,7 +64,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 77);
+            this.label2.Location = new System.Drawing.Point(28, 127);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 25);
             this.label2.TabIndex = 1;
@@ -72,7 +74,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 125);
+            this.label3.Location = new System.Drawing.Point(28, 175);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(155, 25);
             this.label3.TabIndex = 2;
@@ -82,7 +84,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(28, 221);
+            this.label4.Location = new System.Drawing.Point(28, 271);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 25);
             this.label4.TabIndex = 3;
@@ -92,7 +94,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(28, 175);
+            this.label5.Location = new System.Drawing.Point(28, 225);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(79, 25);
             this.label5.TabIndex = 4;
@@ -101,7 +103,7 @@
             // txtEstado
             // 
             this.txtEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEstado.Location = new System.Drawing.Point(219, 175);
+            this.txtEstado.Location = new System.Drawing.Point(219, 225);
             this.txtEstado.Multiline = true;
             this.txtEstado.Name = "txtEstado";
             this.txtEstado.Size = new System.Drawing.Size(268, 25);
@@ -110,7 +112,7 @@
             // txtSumilla
             // 
             this.txtSumilla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSumilla.Location = new System.Drawing.Point(219, 221);
+            this.txtSumilla.Location = new System.Drawing.Point(219, 271);
             this.txtSumilla.Multiline = true;
             this.txtSumilla.Name = "txtSumilla";
             this.txtSumilla.Size = new System.Drawing.Size(268, 176);
@@ -119,7 +121,7 @@
             // txtAñoEdicion
             // 
             this.txtAñoEdicion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAñoEdicion.Location = new System.Drawing.Point(219, 125);
+            this.txtAñoEdicion.Location = new System.Drawing.Point(219, 175);
             this.txtAñoEdicion.Multiline = true;
             this.txtAñoEdicion.Name = "txtAñoEdicion";
             this.txtAñoEdicion.Size = new System.Drawing.Size(268, 25);
@@ -128,7 +130,7 @@
             // txtAutor
             // 
             this.txtAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAutor.Location = new System.Drawing.Point(219, 77);
+            this.txtAutor.Location = new System.Drawing.Point(219, 127);
             this.txtAutor.Multiline = true;
             this.txtAutor.Name = "txtAutor";
             this.txtAutor.Size = new System.Drawing.Size(268, 25);
@@ -137,7 +139,7 @@
             // txtTitulo
             // 
             this.txtTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTitulo.Location = new System.Drawing.Point(219, 24);
+            this.txtTitulo.Location = new System.Drawing.Point(219, 74);
             this.txtTitulo.Multiline = true;
             this.txtTitulo.Name = "txtTitulo";
             this.txtTitulo.Size = new System.Drawing.Size(268, 25);
@@ -146,7 +148,7 @@
             // btnRegistrarLibro
             // 
             this.btnRegistrarLibro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrarLibro.Location = new System.Drawing.Point(33, 441);
+            this.btnRegistrarLibro.Location = new System.Drawing.Point(33, 471);
             this.btnRegistrarLibro.Name = "btnRegistrarLibro";
             this.btnRegistrarLibro.Size = new System.Drawing.Size(204, 41);
             this.btnRegistrarLibro.TabIndex = 13;
@@ -194,7 +196,7 @@
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(283, 441);
+            this.btnEliminar.Location = new System.Drawing.Point(283, 471);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(204, 41);
             this.btnEliminar.TabIndex = 15;
@@ -202,11 +204,38 @@
             this.btnEliminar.UseVisualStyleBackColor = true;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Tipo";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Libro",
+            "Enciclopedia",
+            "Revista",
+            "Best Seller"});
+            this.cboTipo.Location = new System.Drawing.Point(219, 28);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(268, 26);
+            this.cboTipo.TabIndex = 17;
+            this.cboTipo.Text = "Seleccionar Tipo";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1122, 524);
+            this.Controls.Add(this.cboTipo);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.dgLibro);
             this.Controls.Add(this.btnRegistrarLibro);
@@ -247,6 +276,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colAño;
         private System.Windows.Forms.DataGridViewTextBoxColumn colEstado;
         private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboTipo;
     }
 }
 
