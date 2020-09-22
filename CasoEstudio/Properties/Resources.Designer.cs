2a8bb5fe3d3050d99f0c58367b1c4be6489864a1
jossrc <kuro.rc10@gmail.com>
2020-09-22 10:31:23 -0500

    Realizando cambios adicionales

--- CasoEstudio/Properties/Resources.Designer.cs
+++ CasoEstudio/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace CasoEstudio.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CasoEstudio_01.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CasoEstudio.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
